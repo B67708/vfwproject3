bccd3fd3db7c0a62648b6d9f9ba4e684cff752cb
John Cardiff-Winchell <johnwinchell86@fullsail.edu>
2013-03-18 16:28:07 -0400

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,165 @@
+// John Cardiff-Winchell
+// VFW 1303
+// Project week 2
+
+// Wait until DOM is loaded
+window.addEventListener("DOMContentLoaded",function(){
+
+// Function to get elements by id
+function gid(x){
+	var element = document.getElementById(x);
+	return element;
+}
+// Creates form for make drop down menu.
+function dropDown1(){
+	var formTag = document.getElementsByTagName("form"),
+		pickLi = gid("make"),
+		createSelect = document.createElement("select");
+		createSelect.setAttribute("id", "makes");
+		for(var i=0, j=makeList.length; i<j; i++){
+			var makeOption = document.createElement("option");
+			var optionTxt = makeList[i];
+			makeOption.setAttribute("value", optionTxt);
+			makeOption.innerHTML = optionTxt;
+			createSelect.appendChild(makeOption);
+		}		
+		pickLi.appendChild(createSelect)
+}	
+	
+// Creates form for model drop down menu.
+function dropDown2(){
+	var formTag = document.getElementsByTagName("form"),
+		pickLi = gid("model"),
+		createSelect = document.createElement("select");
+		createSelect.setAttribute("id", "models");
+		for(var i=0, j=modelList.length; i<j; i++){
+			var makeOption = document.createElement("option");
+			var optionTxt = modelList[i];
+			makeOption.setAttribute("value", optionTxt);
+			makeOption.innerHTML = optionTxt;
+			createSelect.appendChild(makeOption);
+		}		
+		pickLi.appendChild(createSelect)
+}
+
+//Find value of selected radio button
+function getRadio(){
+	var rad = document.forms[0].newUsed;
+		for(var i=0; i<rad.length; i++){
+			if(rad[i].checked){
+				newUsedValue = rad[i].value;
+			}
+		}
+}
+
+// Making switch case function to toggle links
+
+function toggle(x){
+	switch(x){
+		case "on":
+			gid("vehLib").style.display = "none";
+			gid("clear").style.display = "inline";
+			gid("display").style.display = "none";
+			gid("addCar").style.display = "inline";
+			break;
+		case "off":
+			gid("vehLib").style.display = "block";
+			gid("clear").style.display = "inline";
+			gid("display").style.display = "inline";
+			gid("addCar").style.display = "none";
+			gid("item").style.display = "none";
+			break;
+		default:
+			break;
+			return false;
+		}
+	
+}
+
+// Making save data function
+function submitData(){
+	var id = Math.floor(Math.random()*5551212);
+	
+// Gathering data in an object. Object has array with form label and input label
+	getRadio();
+	var item ={};
+		item.newUsed =["New or Used: ", newUsedValue];
+		item.make =["Make: ", gid("makes").value];
+		item.model =["Model: ", gid("models").value];
+		item.year =["Year: ", gid("year").value];
+		item.mileage =["Mileage: ", gid("mileage").value];
+		item.condition =["Condition: ", gid("condition").value];	
+		item.date =["Date: ", gid("date").value];
+		item.notes =["Notes: ", gid("notes").value];
+		
+// Use stringify to convert object to string and save data to local storage
+	localStorage.setItem(id, JSON.stringify(item));
+	alert("Vehicle added!")
+		
+}
+
+// This function displays data to the browser
+function getData(){
+	toggle("on");
+	if(localStorage.length === 0){
+		alert("Local storage is empty.");
+	}
+	var createDiv = document.createElement("div");
+	createDiv.setAttribute("id", "item");
+	var createUl = document.createElement("ul");
+	createDiv.appendChild(createUl);
+	document.body.appendChild(createDiv);
+	gid("item").style.display = "block";
+	for(var i=0, j=localStorage.length; i<j; i++){
+		var createLi = document.createElement("li");
+		createUl.appendChild(createLi);
+		var key = localStorage.key(i)
+		var value = localStorage.getItem(key);
+		
+// Converts string to object with JSON parse		
+		var object = JSON.parse(value);
+		var subList = document.createElement("ul");
+		createLi.appendChild(subList);
+		for(var y in object){
+		var createSubLi = document.createElement("li");
+		createLi.appendChild(createSubLi);
+		var SubText = object[y] [0] +" "+ object[y] [1];
+		createSubLi.innerHTML = SubText;
+			
+		}			
+	}
+	
+	
+}
+
+//This function clears local storage data
+function clearData(){
+	localStorage.clear();
+	alert("Vehicle deleted!");
+	window.location.reload();
+} 
+	
+// Variable Defaults
+var makeList = ["--Choose a make--", "Chevrolet", "Ford", "Buick", "Dodge", "Jeep"],
+	modelList = ["--Choose a model--", "Cruze", "Impala", "Cobalt", "Camaro", "Focus", "Explorer", "Fusion", "F-150", "Regal", "Lacrosse", "Enclave", "Century", "Viper", "Ram", "Dart", "Durango", "Wrangler", "Liberty", "Patriot", "Compass"],
+	newUsedValue;
+dropDown1();
+dropDown2();
+
+
+
+// Links and submit button
+var display = gid("display");
+display.addEventListener("click", getData);
+
+var clear = gid("clear");
+clear.addEventListener("click", clearData);
+
+var submit = gid("submit");
+submit.addEventListener("click", submitData);
+
+
+	
+
+
+})
